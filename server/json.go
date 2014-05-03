6b7deb39db9ee6d3a3983613f9942e0aa9a289f2
Iwan BK <iwanbk@gmail.com>
2014-05-03 21:40:43 +0700

    remove bitly/simplejson usage

--- server/json.go
+++ server/json.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	simplejson "github.com/bitly/go-simplejson"
+	"encoding/json"
 	log "github.com/ngmoco/timber"
 )
 
 //jsonMarshal marshal map to json string
+//this function is deprecated, we should use EndptMsg.MarshalJson
 func jsonMarshal(event string, data map[string]interface{}) string {
-	js, err := simplejson.NewJson([]byte("{}"))
-	if err != nil {
-		log.Error("jsonMarshal() failed to create simplejson object. Event :" + event + ". error :" + err.Error())
-		return "{}"
-	}
-	js.Set("event", event)
-	js.Set("data", data)
+	em := &EndptMsg{Event: event, Data: data}
 
-	b, err := js.MarshalJSON()
+	b, err := json.Marshal(em)
 	if err != nil {
-		log.Error("jsonMarshal() failed to marshal json. Event :" + event + ". error :" + err.Error())
+		log.Error("jsonMarshal() failed. err =", err.Error())
 		return "{}"
 	}
 	return string(b)
